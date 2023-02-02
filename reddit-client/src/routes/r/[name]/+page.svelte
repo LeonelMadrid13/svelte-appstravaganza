b67cf3c04bcb856d79b071d6b59960cdf2990898
MasterKiri13 <leoneleduardomadrid@hotmail.com>
2023-02-01 17:49:15 -0700

    video and image from posts are now displayed in the post list

--- reddit-client/src/routes/r/[name]/+page.svelte
+++ reddit-client/src/routes/r/[name]/+page.svelte
@@ -5,10 +5,10 @@
   let posts = data.props.posts;
 </script>
 
-<!-- <pre>{JSON.stringify(posts, null, 2)}</pre> -->
 
 {#each posts as post }
 <div class='container'>
+  <!-- <pre>{JSON.stringify(post.data, null, 2)}</pre> -->
   <div class='post'>
     <h4>
       <a 
@@ -18,19 +18,43 @@
         {post.data.title}
       </a>
     </h4>
-    {#if post.data.selftext !== ''}
-      <md-block> {post.data.selftext} </md-block>
-    {/if}
+    <div class='media'>
+      {#if post.data.secure_media?.reddit_video}
+        <video muted autoplay loop src={post.data.secure_media.reddit_video.fallback_url}></video>
+      {:else if post.data.preview?.reddit_video_preview}
+        <video muted autoplay loop src={post.data.preview.reddit_video_preview.fallback_url}></video>
+      {:else if post.data.post_hint === 'image'}
+        <img src={post.data.url} alt="">
+      {/if}
+    </div>
   </div>
 </div>
 {/each}
 
 <style>
   .post{
+    display: flex;
     outline: 2px solid white;
     padding: 1rem;
     margin: 1rem;
     width: 750px;
+  }
+  video{
+    width: 100%;
+    height: 100%;
+  }
+
+  img{
+    width: auto;
+    height: 100%;
+  }
+
+  .media{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 350px;
   }
   .container{
     display: flex;
